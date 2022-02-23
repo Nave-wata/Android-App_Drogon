d2ba29c1e1c8452a7f3ceb251bcfd0a0da2ed5b8
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-23 22:11:19 +0900

    Add controllers

---
+++ drogon_test/testctrl.cc
@@ -0,0 +1,6 @@
+#include "testctrl.h"
+
+void testctrl::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+}
